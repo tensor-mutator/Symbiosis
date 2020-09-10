d46ee629cf81e115867f9f6d8737d6a07d34becf
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-09-11 04:23:02 +0530

    Update __init__.py

--- generalized-rl/generalized_rl/__init__.py
+++ generalized-rl/generalized_rl/__init__.py
@@ -1,6 +1,6 @@
 __version__ = '0.1.0'
 
 from .environment import Environment
-from .Agents.DQN.dqn import DQN
+from .Agents.DQN.ddqn import DDQN
 
-__all__ = ["Environment", "DQN"]
+__all__ = ["Environment", "DDQN"]
