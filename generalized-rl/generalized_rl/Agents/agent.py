14185894bd3614190e740e55a668023285c36bac
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-09-13 21:34:31 +0530

    Update agent.py

--- generalized-rl/generalized_rl/Agents/agent.py
+++ generalized-rl/generalized_rl/Agents/agent.py
@@ -1,6 +1,20 @@
 from abc import ABCMeta, abstractmethod
+from typing import Dict
 
 class Agent(metaclass=ABCMeta):
 
       @abstractmethod
+      def run(self) -> None:
+          ...
       
+      @abstractmethod
+      def _get_network_params(self, hyperparams: Dict) -> Dict:
+          ...
+
+      @abstractmethod
+      def _train(self) -> float:
+          ...
+
+      @abstractmethod
+      def _mutate_alias(self, alias: str, hyperparams: Dict) -> str:
+          ...
