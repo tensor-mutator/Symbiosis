2f5e8b32e06278b7952148030e36ed92358420ff
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-09-17 02:24:45 +0530

    Update agent.py

--- generalized-rl/generalized_rl/Agents/agent.py
+++ generalized-rl/generalized_rl/Agents/agent.py
@@ -1,30 +1,55 @@
 from abc import ABCMeta, abstractmethod
 from typing import Dict
+import tensorflow as tf
+import os
+from .utilities import Progress
 
 class Agent(metaclass=ABCMeta):
 
       @abstractmethod
       def run(self) -> None:
           ...
-      
-      @abstractmethod
-      def _get_network_params(self, hyperparams: Dict) -> Dict:
-          ...
 
       @abstractmethod
-      def _train(self) -> float:
+      def train(self) -> float:
           ...
 
       @property
-      @abstractmethod
+      def session(self) -> tf.Session:
+          return self._session
+
+      @property
+      def graph(self) -> tf.Graph:
+          retrun self._graph
+
+      @property
       def alias(self) -> str:
-          ...
+          return self._alias
+
+      @property
+      def progress(self) -> Progress:
+          return self._progress
+
+      @property
+      def env(self) -> str:
+          return self._env.name
       
-      def _save_loss(self, loss: float) -> None:
+      def save_loss(self, loss: float) -> None:
           pass
 
-      def _save(self) -> None:
-          pass
+      def save(self) -> None:
+          with self._graph.as_default():
+               saver = tf.train.Saver(max_to_keep=5)
+          saver.save(self.session, os.path.join(self.workspace(), "{}.ckpt".format(self.alias)))
 
-      def _load(self) -> None:
-          pass
+      def load(self) -> None:
+          with self._graph.as_default():
+               saver = tf.train.Saver(max_to_keep=5)
+          ckpt = tf.train.get_checkpoint_state(self.workspace())
+          saver.restore(self.session, ckpt.model_checkpoint_path)
+
+      def workspace(self) -> str:
+          path = os.path.join(self.env, self.alias)
+          if not os.path.exists(path):
+             os.makedirs(path)
+          return path
