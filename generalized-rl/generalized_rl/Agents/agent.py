b7054562c317dc1878ff33fa79c29ae36e1af02f
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-09-20 17:41:04 +0530

    Update agent.py

--- generalized-rl/generalized_rl/Agents/agent.py
+++ generalized-rl/generalized_rl/Agents/agent.py
@@ -2,6 +2,7 @@ from abc import ABCMeta, abstractmethod
 from typing import Dict, Any
 import tensorflow as tf
 import os
+import dill
 from .utilities import Progress
 
 class Agent(metaclass=ABCMeta):
@@ -43,9 +44,19 @@ class Agent(metaclass=ABCMeta):
           pass
 
       def save(self) -> None:
+          path = self.workspace()
           with self._graph.as_default():
                saver = tf.train.Saver(max_to_keep=5)
-          saver.save(self.session, os.path.join(self.workspace(), "{}.ckpt".format(self.alias)))
+          saver.save(self.session, os.path.join(path, "{}.ckpt".format(self.alias)))
+          with open(os.path.join(path, "{}.progress".format(self.alias)), "wb") as f_obj:
+               dill.dump(self._progress, f_obj, protocol=dill.HIGHEST_PROTOCOL)
+
+      def load_progress(self) -> None:
+          path = self.workspace()
+          if os.path.exists(os.path.join(path, "{}.progress".format(self.alias))):
+             with open(os.path.join(path, "{}.progress".format(self.alias)), "rb") as f_obj:
+                  return dill.load(f_obj)
+          return Progress(self._observe, self._explore)
 
       def load(self) -> None:
           with self._graph.as_default():
