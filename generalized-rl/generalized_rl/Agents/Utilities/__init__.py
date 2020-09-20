e9aff02f0b3c7ec4451aa61d89f5457277551749
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-09-20 21:30:52 +0530

    Update __init__.py

--- generalized-rl/generalized_rl/Agents/Utilities/__init__.py
+++ generalized-rl/generalized_rl/Agents/Utilities/__init__.py
@@ -2,5 +2,6 @@ from .lr_scheduler import LRScheduler
 from .progress import Progress
 from .greedy_epsilon import GreedyEpsilon
 from .exceptions import Exceptions
+from .reward_manager import RewardManager
 
-__all__ = ["LRScheduler", "Progress", "GreedyEpsilon", "Exceptions"]
+__all__ = ["LRScheduler", "Progress", "GreedyEpsilon", "Exceptions", "RewardManager"]
