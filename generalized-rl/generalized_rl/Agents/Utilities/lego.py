7436310ad9af1c41a9348b35686f5b9282d55036
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-09-21 02:51:20 +0530

    Create lego.py

---
+++ generalized-rl/generalized_rl/Agents/Utilities/lego.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+import tensorflow.keras.layers as layers
+from typing import Callable, Tuple
+
+class NetBlocks:
+
+      graph = tf.get_default_graph()
+
+      @staticmethod
+      def Conv2D(filters: int, kernel_size: Tuple[int, int], strides: Tuple[int, int],
+                 activation: str = "relu", batch_norm: bool = False, time_distributed: bool = False) -> Callable:
+          def op(tensor: tf.Tensor) -> tf.Tensor:
+              if time_distributed:
+                 tensor_out = layers.TimeDistributed(layers.Conv2D(filters=filters, kernel_size=kernel_size, strides=strides))(tensor)
+              else:
+                 tensor_out = layers.Conv2D(filters=filters, kernel_size=kernel_size, strides=strides)(tensor)
+              if batch_norm:
+                 if time_distributed:
+                    tensor_out = layers.TimeDistributed(layers.BatchNomalization())(tensor_out)
+                 else:
+                    tensor_out = layers.BatchNormalization()(tensor_out)
+              if time_distributed:
+                 tensor_out = layers.TimeDistributed(layers.Activation(activation))(tensor_out)
+              else:
+                 tensor_out = layers.Activation(activation)(tensor_out)
+              return tensor_out
+          return op
+
+      @staticmethod
