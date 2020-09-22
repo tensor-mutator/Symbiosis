8061863471c125c7c75ed285bdbc462fbe6a8ddf
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-09-23 00:25:01 +0530

    Update lego.py

--- generalized-rl/generalized_rl/Agents/Utilities/lego.py
+++ generalized-rl/generalized_rl/Agents/Utilities/lego.py
@@ -13,7 +13,7 @@ class NetBlocks:
           return NetBlocks.graph.get_operation_by_name('{}'.format(name)).outputs[0]
 
       @staticmethod
-      def Conv2D(filters: int, kernel_size: Tuple[int, int], strides: Tuple[int, int],
+      def Conv2D(filters: int, kernel_size: Tuple[int, int], strides: Tuple[int, int] = (1,1,),
                  activation: str = "relu", batch_norm: bool = False, time_distributed: bool = False) -> Callable:
           def _op(tensor: tf.Tensor) -> tf.Tensor:
               if time_distributed:
@@ -55,12 +55,25 @@ class NetBlocks:
       def Conv2DNature(batch_norm: bool = False, time_distributed: bool = False) -> Callable:
           def _op(tensor: tf.Tensor) -> tf.Tensor:
               conv1 = NetBlocks.Conv2D(filters=32, kernel_size=(8, 8), strides=(4, 4),
-                                         batch_norm=batch_norm, time_distributed=time_distributed)(tensor)
-              conv2 = Manipulator.Conv2D(filters=64, kernel_size=(4, 4), strides=(2, 2),
-                                         batch_norm=batch_norm, time_distributed=time_distributed)(conv1)
-              conv3 = Manipulator.Conv2D(filters=64, kernel_size=(3, 3), batch_norm=batch_norm,
-                                         time_distributed=time_distributed)(conv2)
+                                       batch_norm=batch_norm, time_distributed=time_distributed)(tensor)
+              conv2 = NetBlocks.Conv2D(filters=64, kernel_size=(4, 4), strides=(2, 2),
+                                       batch_norm=batch_norm, time_distributed=time_distributed)(conv1)
+              conv3 = NetBlocks.Conv2D(filters=64, kernel_size=(3, 3), batch_norm=batch_norm,
+                                       time_distributed=time_distributed)(conv2)
               flattened = layers.TimeDistributed(layers.Flatten())(conv3) if timedistributed else layers.Flatten()(conv3)
               return NetBlocks.Dense(units=256, batch_norm=batch_norm, time_distributed=time_distributed)(flattened)
           return _op
-      
+
+      @staticmethod
+      def huber_loss(errors: tf.Tensor) -> Tf.Tensor:
+          return tf.where(tf.abs(errors) < 1.0, tf.square(errors) * 0.5, 1.0 * (tf.abs(errors) - 0.5 * 1.0))
+
+      @staticmethod
+      def clip_grads_by_norm(gradients: tf.Tensor, clip_norm: float) -> None:
+          for i, (grad, var) in enumerate(gradients):
+              if grad is not None:
+                 gradients[i] = (tf.clip_by_norm(grad, clip_norm), var)
+
+      @staticmethod
+      def placeholder(shape: tf.DType, name: str = None) -> tf.placeholder:
+          return tf.placeholder(dtype=tf.float32, shape=[None] + list(shape), name=name)
