f7d2aef89a8e4486a3f2b74c8a568f24a6cfa9e6
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-09-23 04:58:56 +0530

    Update network.py

--- generalized-rl/generalized_rl/Agents/DQN/network.py
+++ generalized-rl/generalized_rl/Agents/DQN/network.py
@@ -21,11 +21,11 @@ class DQNNet(NetworkBaseDQN):
           self._importance_sampling_weights = NetBlocks.placeholder(shape=[1])
           self._learning_rate = NetBlocks.placeholder(shape="scalar")
           mask_tensor = tf.one_hot(indices=self._actions, depth=action_size)
-          q_target = tf.reduce_sum(mask_tensor * self._q_target, axis=1)
-          q_predicted = tf.reduce_sum(mask_tensor * self._q_predicted, axis=1)
+          q_target = tf.reduce_sum(mask_tensor*self._q_target, axis=1)
+          q_predicted = tf.reduce_sum(mask_tensor*self._q_predicted, axis=1)
           self._error = tf.subtract(q_predicted, q_target)
           huber_errors = NetBlocks.huber_loss(self._error)
-          self._loss = tf.reduce_mean(self._importance_sampling_weights * huber_errors)
+          self._loss = tf.reduce_mean(self._importance_sampling_weights*huber_errors)
           optimizer = tf.train.AdamOptimizer(learning_rate=self._learning_rate)
           if clip_norm is not None:
              gradients = optimizer.compute_gradients(self._loss, var_list=tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, "local"))
@@ -53,11 +53,11 @@ class DRQNNet(NetworkBaseDQN):
           self._importance_sampling_weights = NetBlocks.placeholder(shape=[1])
           self._learning_rate = NetBlocks.placeholder(shape="scalar")
           mask_tensor = tf.one_hot(indices=self._actions, depth=action_size)
-          q_target = tf.reduce_sum(mask_tensor * self._q_target, axis=1)
-          q_predicted = tf.reduce_sum(mask_tensor * self._q_predicted, axis=1)
+          q_target = tf.reduce_sum(mask_tensor*self._q_target, axis=1)
+          q_predicted = tf.reduce_sum(mask_tensor*self._q_predicted, axis=1)
           self._error = tf.subtract(q_predicted, q_target)
           huber_errors = NetBlocks.huber_loss(self._error)
-          self._loss = tf.reduce_mean(self._importance_sampling_weights * huber_errors)
+          self._loss = tf.reduce_mean(self._importance_sampling_weights*huber_errors)
           optimizer = tf.train.AdamOptimizer(learning_rate=self._learning_rate)
           if clip_norm is not None:
              gradients = optimizer.compute_gradients(self._loss, var_list=tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, "local"))
@@ -81,7 +81,7 @@ class DuelingDQNNet(NetworkBaseDQN):
       def _fuse_adv_val(advantage_tensor: tf.Tensor, value_tensor: tf.Tensor, action_size: int) -> tf.Tensor:
           state_value = NetBlocks.Dense(units=1, activation="linear")(value_tensor)
           action_advantage = NetBlocks.Dense(units=action_size, activation="linear")(advantage_tensor)
-          action_advantage = action_advantage - tf.reduce_mean(action_advantage, axis=1, keep_dims=True)
+          action_advantage = action_advantage-tf.reduce_mean(action_advantage, axis=1, keep_dims=True)
           return tf.add(state_value, action_advantage)
 
       def _build_training_ops(self, action_size: int, clip_norm: float) -> tf.Tensor:
@@ -90,11 +90,11 @@ class DuelingDQNNet(NetworkBaseDQN):
           self._importance_sampling_weights = NetBlocks.placeholder(shape=[1])
           self._learning_rate = NetBlocks.placeholder(shape="scalar")
           mask_tensor = tf.one_hot(indices=self._actions, depth=action_size)
-          q_target = tf.reduce_sum(mask_tensor * self._q_target, axis=1)
-          q_predicted = tf.reduce_sum(mask_tensor * self._q_predicted, axis=1)
+          q_target = tf.reduce_sum(mask_tensor*self._q_target, axis=1)
+          q_predicted = tf.reduce_sum(mask_tensor*self._q_predicted, axis=1)
           self._error = tf.subtract(q_predicted, q_target)
           huber_errors = NetBlocks.huber_loss(self._error)
-          self._loss = tf.reduce_mean(self._importance_sampling_weights * huber_errors)
+          self._loss = tf.reduce_mean(self._importance_sampling_weights*huber_errors)
           optimizer = tf.train.AdamOptimizer(learning_rate=self._learning_rate)
           if clip_norm is not None:
              gradients = optimizer.compute_gradients(self._loss, var_list=tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, "local"))
