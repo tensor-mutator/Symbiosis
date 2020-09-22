c715e5f3456f4932dfdc3a4fe99a7bf21af8f12b
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-09-23 02:56:04 +0530

    Create network_base.py

---
+++ generalized-rl/generalized_rl/Agents/network_base.py
@@ -0,0 +1,36 @@
+from abc import ABCMeta, abstractmethod
+import tensorflow as tf
+
+class NetworkBaseDQN:
+
+      @property
+      def state(self) -> tf.Tensor:
+          return self._state
+
+      @property
+      def action(self) -> tf.Tensor:
+          return self._action
+
+      @property
+      def q_predicted(self) -> tf.Tensor:
+          return self._predicted
+
+      @property
+      def q_target(self) -> tf.Tensor:
+          return self._q_target
+
+      @property
+      def learning_rate(self) -> float:
+          return self._learning_rate
+
+      @property
+      def grad(self) -> tf.Tensor:
+          return self._grad
+
+      @property
+      def error(self) -> tf.Tensor:
+          return self._error
+
+      @property
+      def loss(self) -> tf.Tensor:
+          return self._loss
