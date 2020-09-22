0b3cd19884afe7938409871e4a771db667fe9cd6
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-09-23 02:49:50 +0530

    Update __init__.py

--- generalized-rl/generalized_rl/Agents/__init__.py
+++ generalized-rl/generalized_rl/Agents/__init__.py
@@ -1,5 +1,5 @@
 from .DQN.ddqn import DDQN
 from .agent import Agent
-from .network_base import NetworkBase
+from .network_base import NetworkBaseDQN
 
-__all__ = ["DDQN", "NetworkBase"]
+__all__ = ["DDQN", "NetworkBaseDQN"]
