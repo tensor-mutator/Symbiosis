3375af19333734dd579bc3a76864eb113ca0e8d8
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-09-24 21:10:34 +0530

    Update __init__.py

--- generalized-rl/generalized_rl/__init__.py
+++ generalized-rl/generalized_rl/__init__.py
@@ -1,8 +1,8 @@
 __version__ = '0.1.0'
 
-from .environment import Environment
+from .environment import Environment, State, Action
 from .Agents.DQN.ddqn import DDQN
 from .Agents.DQN.network import DQNNet, DRQNNet, DuelingDQNNet
 from .Agents.network_base import NetworkBaseDQN
 
-__all__ = ["Environment", "DDQN", "NetworkBaseDQN", "DQNNet", "DRQNNet", "DuelingDQNNet"]
+__all__ = ["Environment", "State", "Action", "DDQN", "NetworkBaseDQN", "DQNNet", "DRQNNet", "DuelingDQNNet"]
