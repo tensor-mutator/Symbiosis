47c07d8171a39446b85d4dd261d7e511e8d1ef92
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-09-25 18:44:21 +0530

    Update __init__.py

--- generalized-rl/generalized_rl/Agents/__init__.py
+++ generalized-rl/generalized_rl/Agents/__init__.py
@@ -2,5 +2,6 @@ from .DQN.ddqn import DDQN
 from .DQN.network import DQNNet, DRQNNet, DuelingDQNNet
 from .agent import Agent
 from .network_base import NetworkBaseDQN
+from .Utilities.lego import NetBlocks
 
-__all__ = ["DDQN", "NetworkBaseDQN", "DQNNet", "DRQNNet", "DuelingDQNNet"]
+__all__ = ["DDQN", "NetworkBaseDQN", "DQNNet", "DRQNNet", "DuelingDQNNet", "NetBlocks"]
