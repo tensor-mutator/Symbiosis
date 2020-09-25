ae1bef6cd857efb899b3171af9f46b8f46b446cd
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-09-25 18:40:21 +0530

    Update config.py

--- generalized-rl/generalized_rl/config.py
+++ generalized-rl/generalized_rl/config.py
@@ -3,18 +3,19 @@ from enum import IntEnum, unique
 @unique
 class config(IntEnum):
 
-      DEFAULT: bin = 0b00000000000
-      VERBOSE_LITE: bin = 0b00000000001
-      VERBOSE_HEAVY: bin = 0b00000000010
-      LOG_SUMMARY: bin = 0b00000000100
-      REWARD_EVENT: bin = 0b00000001000
-      LOSS_EVENT: bin = 0b00000010000
-      EPSILON_EVENT: bin = 0b00000100000
-      BETA_EVENT: bin = 0b00001000000
-      LR_EVENT: bin = 0b00010000000
-      SAVE_WEIGHTS: bin = 0b00100000000
-      LOAD_WEIGHTS: bin = 0b01000000000
-      NOHUP: bin = 0b10000000000
+      DEFAULT: bin = 0b000000000000
+      VERBOSE_LITE: bin = 0b000000000001
+      VERBOSE_HEAVY: bin = 0b000000000010
+      LOG_SUMMARY: bin = 0b000000000100
+      REWARD_EVENT: bin = 0b000000001000
+      LOSS_EVENT: bin = 0b000000010000
+      EPSILON_EVENT: bin = 0b000000100000
+      BETA_EVENT: bin = 0b000001000000
+      LR_EVENT: bin = 0b000010000000
+      SAVE_WEIGHTS: bin = 0b000100000000
+      LOAD_WEIGHTS: bin = 0b001000000000
+      SAVE_FRAMES: bin = 0b010000000000
+      NOHUP: bin = 0b100000000000
 
       @staticmethod
       def show() -> None:
@@ -29,4 +30,5 @@ class config(IntEnum):
           print("config.LR_EVENT")
           print("config.SAVE_WEIGHTS")
           print("config.LOAD_WEIGHTS")
+          print("config.SAVE_FRAMES")
           print("config.NOHUP")
