3709f1d7c9273ef6d8f0aa0754556c4f3a99e643
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-09-25 18:05:35 +0530

    Update config.py

--- generalized-rl/generalized_rl/config.py
+++ generalized-rl/generalized_rl/config.py
@@ -1,5 +1,6 @@
-from enum import IntEnum
+from enum import IntEnum, unique
 
+@unique
 class config(IntEnum):
 
       DEFAULT: bin = 0b00000000000
@@ -14,3 +15,18 @@ class config(IntEnum):
       SAVE_WEIGHTS: bin = 0b00100000000
       LOAD_WEIGHTS: bin = 0b01000000000
       NOHUP: bin = 0b10000000000
+
+       @staticmethod
+      def show() -> None:
+          print("config.DEFAULT")
+          print("config.VERBOSE_LITE")
+          print("config.VERBOSE_HEAVY")
+          print("config.LOG_SUMMARY")
+          print("config.REWARD_EVENT")
+          print("config.LOSS_EVENT")
+          print("config.EPSILON_EVENT")
+          print("config.BETA_EVENT")
+          print("config.LR_EVENT")
+          print("config.SAVE_WEIGHTS")
+          print("config.LOAD_WEIGHTS")
+          print("config.NOHUP")
