30bb54adb195056e670d8ea26255d6f59d0d2da0
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-09-25 17:02:31 +0530

    Update config.py

--- generalized-rl/generalized_rl/config.py
+++ generalized-rl/generalized_rl/config.py
@@ -2,10 +2,14 @@ from enum import IntEnum
 
 class config(IntEnum):
 
-      DEFAULT: bin = 0b000000
-      CONSOLE_SUMMARY: bin = 0b000001
-      LOG_SUMMARY: bin = 0b000010
-      TENSOR_EVENT: bin = 0b000100
-      SAVE_WEIGHTS: bin = 0b001000
-      LOAD_WEIGHTS: bin = 0b010000
-      NOHUP: bin = 0b100000
+      DEFAULT: bin = 0b0000000000
+      CONSOLE_SUMMARY: bin = 0b0000000001
+      LOG_SUMMARY: bin = 0b0000000010
+      REWARD_EVENT: bin = 0b0000000100
+      LOSS_EVENT: bin = 0b0000001000
+      EPSILON_EVENT: bin = 0b0000010000
+      BETA_EVENT: bin = 0b0000100000
+      LR_EVENT: bin = 0b0001000000
+      SAVE_WEIGHTS: bin = 0b0010000000
+      LOAD_WEIGHTS: bin = 0b0100000000
+      NOHUP: bin = 0b1000000000
