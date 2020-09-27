b1e8784c13d37d53cfe09336845b2838239a18b2
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-09-28 01:28:48 +0530

    Update __init__.py

--- generalized-rl/generalized_rl/__init__.py
+++ generalized-rl/generalized_rl/__init__.py
@@ -1,11 +1,13 @@
 __version__ = '0.1.0'
 
 from .environment import Environment, State, Action
+from .Agents.flow_base import Flow
 from .Agents.DQN.ddqn import DDQN
 from .Agents.DQN.network import DQNNet, DRQNNet, DuelingDQNNet
 from .Agents.network_base import NetworkBaseDQN
 from .Agents.Utilities.lego import NetBlocks
+from .Agents.Utilities.flow import LucasKanadeFlow
 from .config import config
 
 __all__ = ["Environment", "State", "Action", "DDQN", "NetworkBaseDQN", "DQNNet", "DRQNNet", "DuelingDQNNet",
-           "config", "NetBlocks"]
+           "config", "NetBlocks", "LucasKanadeFlow"]
