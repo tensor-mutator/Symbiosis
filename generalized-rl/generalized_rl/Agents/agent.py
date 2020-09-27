4c0a91616f2444ce6d44c1a5b3526bf487bb6519
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-09-28 04:01:21 +0530

    Update agent.py

--- generalized-rl/generalized_rl/Agents/agent.py
+++ generalized-rl/generalized_rl/Agents/agent.py
@@ -29,8 +29,7 @@ def record(func: Callable) -> Callable:
     def inner(inst, frame, *args, **kwargs) -> List:
         if inst.config & config.SAVE_FRAMES:
            cv2.imwrite(inst._frame_inventory.path, frame)
-        state = func(inst, frame, *args, **kwargs)
-        return state, inst._frame_inventory.path
+        return func(inst, frame, *args, **kwargs), inst._frame_inventory.path
     return inner
 
 class Agent(metaclass=ABCMeta):
