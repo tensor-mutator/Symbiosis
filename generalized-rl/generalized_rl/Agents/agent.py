742a2de8c4ad5379b5797df0b1d466b75d4e66de
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-09-28 02:02:55 +0530

    Update agent.py

--- generalized-rl/generalized_rl/Agents/agent.py
+++ generalized-rl/generalized_rl/Agents/agent.py
@@ -10,6 +10,8 @@ import numpy as np
 import os
 import dill
 import cv2
+import json
+from .flow_base import Flow
 from .DQN.replay import ExperienceReplay
 from .Utilities import Progress, RewardManager, Inventory
 from .Utilities.exceptions import *
@@ -26,7 +28,7 @@ def register(suite: str) -> Callable:
 def record(func) -> Callable:
     def inner(inst, frame, *args, **kwargs) -> np.ndarray:
         if inst.config & config.SAVE_FRAMES:
-           cv2.imwrite(inst._inventory.path, frame)
+           cv2.imwrite(inst._frame_inventory.path, frame)
         return func(inst, frame, *args, **kwargs)
     return inner
 
@@ -45,8 +47,9 @@ class Agent(metaclass=ABCMeta):
                            reward_manager: RewardManager) -> Generator:
           env.make()
           env.reset()
-          state = self.state(env.render())
-          yield state
+          image = env.render()
+          state = self.state(image)
+          yield image, state
           env.close()
           reward_manager.rollout()
           if self.config & config.SAVE_WEIGHTS:
@@ -54,13 +57,15 @@ class Agent(metaclass=ABCMeta):
           progress.bump_episode()
 
       def _episode_suite_dqn(self) -> None:
-          with self._episode_context(self.env, self.progress, self._reward_manager) as s_t:
+          with self._episode_context(self.env, self.progress, self._reward_manager) as [x_t, s_t]:
                while not self.env.ended and self.progress.clock < self.total_steps:
                      a_t = self.action(s_t)
                      x_t1, r_t, done, _ = self.env.step(a_t)
+                     self._save_flow(x_t, x_t1, r_t)
                      self._reward_manager.update(r_t)
                      s_t1 = self.state(x_t1, s_t)
                      self.replay.add((s_t, a_t, r_t, s_t1, done,))
+                     x_t = x_t1
                      s_t = s_t1
                      if self.progress.explore_clock and self.progress.training_clock%self.training_interval == 0:
                         self.train()
@@ -81,11 +86,27 @@ class Agent(metaclass=ABCMeta):
           if not suite:
              raise MissingSuiteError("Matching suite not found for class: {}".format(self.__class__.__name__))
           self._reward_manager = RewardManager(self.env, self.alias, self.config, self.progress)
-          if self.config & config.SAVE_FRAMES:
-             self._inventory = Inventory("FRAMES", "frame", "PNG", self.env, self.alias, self.progress)
+          self._initiate_inventories()
           self._load_artifacts()
           while self.progress.clock < self.total_steps:
                 suite()
+
+      def _initiate_inventories(self) -> None:
+          if self.config & config.SAVE_FRAMES:
+             self._frame_inventory = Inventory("FRAMES", "frame", "PNG", self.env, self.alias, self.progress)
+          if self.config & config.SAVE_FLOW:
+             self._flow_inventory = Inventory("FLOW", "flow", "PNG", self.env, self.alias, self.progress)
+
+      def _save_flow(self, x_t: np.ndarray, x_t1: np.ndarray, r_t: float) -> None:
+          if self.config & config.SAVE_FLOW:
+             cv2.imwrite(self._flow_inventory.path, self.flow.flow_map(x_t, x_t1))
+             flow_rewards = dict()
+             if os.path.exists(os.path.join(self._flow_inventory.inventory_path, "rewards.meta")):
+                with open(os.path.join(self._flow_inventory.inventory_path, "rewards.meta"), "r") as f_obj:
+                     flow_rewards = json.load(f_obj)
+             flow_rewards.update({self._flow_inventory.path: r_t})
+             with open(os.path.join(self._flow_inventory.inventory_path, "rewards.meta"), "w") as f_obj:
+                     json.dump(flow_rewards, f_obj)
 
       @abstractmethod
       def train(self) -> float:
@@ -188,3 +209,7 @@ class Agent(metaclass=ABCMeta):
       @property
       def explore(self) -> float:
           return getattr(self, "_explore", np.inf)
+
+     @property
+     def flow(self) -> Flow:
+         return self._flow
