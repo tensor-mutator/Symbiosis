72e01f099a1ede3d9178219acb477bb304c55882
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-09-28 02:09:08 +0530

    Create lucas_kanade_flow.py

---
+++ generalized-rl/generalized_rl/Agents/Utilities/lucas_kanade_flow.py
@@ -0,0 +1,8 @@
+import cv2
+import numpy as np
+from ..flow_base import Flow
+
+class LucasKanadeFlow(Flow):
+
+      def flow_map(self, x_t: np.ndarray, x_t1: np.ndarray) -> np.ndarray:
+          pass
