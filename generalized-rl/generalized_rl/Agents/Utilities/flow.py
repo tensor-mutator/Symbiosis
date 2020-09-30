c917b37ae246bad1c6b7b3d50fed0adf385ea4c0
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-09-30 06:54:05 +0530

    Update flow.py

--- generalized-rl/generalized_rl/Agents/Utilities/flow.py
+++ generalized-rl/generalized_rl/Agents/Utilities/flow.py
@@ -12,10 +12,7 @@ class GunnerFarnebackFlow(Flow):
           x_t_gray = cv2.cvtColor(x_t,cv2.COLOR_BGR2GRAY)
           x_t1_gray = cv2.cvtColor(x_t1,cv2.COLOR_BGR2GRAY)
           flow = cv2.calcOpticalFlowFarneback(x_t_gray, x_t1_gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
-          mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
-          flow_map[..., 0] = ang*180/np.pi/2
-          flow_map[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
-          return cv2.cvtColor(flow_map, cv2.COLOR_HSV2BGR)
+          return self.flow_to_image(flow)
 
 class LucasKanadeFlow(Flow):
 
