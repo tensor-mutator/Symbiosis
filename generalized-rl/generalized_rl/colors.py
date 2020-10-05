d2b01b5957cd6b36160c7c85a475a77ceeebc387
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-10-06 01:28:21 +0530

    Create colors.py

---
+++ generalized-rl/generalized_rl/colors.py
@@ -0,0 +1,15 @@
+class COLORS:
+
+      DEFAULT: str = "\033[0m"
+      RED: "\033[31m"
+      GREEN = "\033[32m"
+      CYAN = "\033[36m"
+      MAGENTA = "\033[35m"
+      YELLOW = "\033[33m"
+      BLUE = "\033[34m"
+      BOLD_RED: "\033[91m"
+      BOLD_GREEN = "\033[92m"
+      BOLD_CYAN = "\033[96m"
+      BOLD_MAGENTA = "\033[95m"
+      BOLD_YELLOW = "\033[93m"
+      BOLD_BLUE = "\033[94m"
