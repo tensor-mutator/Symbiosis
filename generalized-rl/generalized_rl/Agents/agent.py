1284685928955fbc0ddcafe79fb3bcb51ffe43b7
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-10-07 03:46:31 +0530

    Update agent.py

--- generalized-rl/generalized_rl/Agents/agent.py
+++ generalized-rl/generalized_rl/Agents/agent.py
@@ -1,5 +1,5 @@
 from abc import ABCMeta, abstractmethod
-from typing import Dict, Any, Generator, Callable, List
+from typing import Dict, Any, Generator, Callable
 from contextlib import contextmanager, suppress
 from glob import glob
 import warnings
@@ -13,50 +13,15 @@ import cv2
 import json
 import signal
 from collections import deque
-from ..colors import COLORS
+from .decorators import record, register_handler, track
 from .flow_base import Flow
 from .network_base import NetworkMeta
 from .DQN.replay import ExperienceReplay
 from .Utilities import Progress, RewardManager, Inventory
 from .Utilities.exceptions import *
+from ..colors import COLORS
 from ..environment import Environment
 from ..config import config
-
-def register(suite: str) -> Callable:
-    def wrapper(func: Callable) -> Callable:
-        def run(inst: "<Agent inst>") -> None:
-            super(inst.__class__, inst).run(getattr(inst, suite))
-        return run
-    return wrapper
-
-def record(func: Callable) -> Callable:
-    def inner(inst: "<Agent inst>", frame: np.ndarray, state: Any = None) -> List:
-        path = None
-        if inst.config & (config.SAVE_FRAMES+config.SAVE_FLOW):
-           if state is None:
-              path = inst._frame_inventory.init_path
-           else:
-              path = inst._frame_inventory.path
-           cv2.imwrite(path, inst.env.state.frame)
-        return func(inst, frame, state), path
-    return inner
-
-def register_handler(unix_signals: List) -> Callable:
-    def outer(handler: Callable) -> Callable:
-        def inner(inst: "<Agent inst>", signal_id: int = None, frame: Any = None) -> None:
-            for sig in unix_signals:
-                signal.signal(sig, lambda x, y: handler(inst, x, y))
-        return inner
-    return outer
-
-def track(network: NetworkMeta, config: bin = config.DEFAULT, flow: Flow = None) -> Callable:
-    def outer(func: Callable) -> Callable:
-        def inner(inst, env: Environment, network: NetworkMeta = network, config: bin = config,
-                  flow: Flow = flow, **hyperparams) -> None:
-            inst._params = dict(env=env, network=network, config=config, flow=flow)
-            func(inst, env, network, config, flow, **hyperparams)
-        return inner
-    return outer
 
 UNIX_SIGNALS = [signal.SIGABRT, signal.SIGBUS, signal.SIGHUP, signal.SIGILL, signal.SIGINT,
                 signal.SIGQUIT, signal.SIGTERM, signal.SIGTRAP, signal.SIGTSTP]
