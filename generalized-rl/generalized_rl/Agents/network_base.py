2fad4a37a111f2626ed53383d0cf2f2a1314100b
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-10-09 19:01:51 +0530

    Update network_base.py

--- generalized-rl/generalized_rl/Agents/network_base.py
+++ generalized-rl/generalized_rl/Agents/network_base.py
@@ -1,8 +1,5 @@
 from typing import Dict, Tuple
-import warnings
-with warnings.catch_warnings():  
-     warnings.filterwarnings("ignore", category=FutureWarning)
-     import tensorflow.compat.v1 as tf
+import tensorflow.compat.v1 as tf
 
 class NetworkMeta(type):
 
