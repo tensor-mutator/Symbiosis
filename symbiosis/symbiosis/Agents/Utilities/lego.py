bc1c098ba5e768b70f03c6c7a36eb7c1457c1538
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-10-11 05:57:10 +0530

    Update lego.py

--- symbiosis/symbiosis/Agents/Utilities/lego.py
+++ symbiosis/symbiosis/Agents/Utilities/lego.py
@@ -15,80 +15,6 @@ class NetBlocks:
           return NetBlocks.graph.get_operation_by_name('{}'.format(name)).outputs[0]
 
       @staticmethod
-      def Conv2D(filters: int, kernel_size: Tuple[int, int], strides: Tuple[int, int] = (1,1,),
-                 activation: str = "relu", batch_norm: bool = False, time_distributed: bool = False) -> Callable:
-          def _op(tensor: tf.Tensor) -> tf.Tensor:
-              if time_distributed:
-                 tensor_out = layers.TimeDistributed(layers.Conv2D(filters=filters, kernel_size=kernel_size, strides=strides))(tensor)
-              else:
-                 tensor_out = layers.Conv2D(filters=filters, kernel_size=kernel_size, strides=strides)(tensor)
-              if batch_norm:
-                 if time_distributed:
-                    tensor_out = layers.TimeDistributed(layers.BatchNomalization())(tensor_out)
-                 else:
-                    tensor_out = layers.BatchNormalization()(tensor_out)
-              if time_distributed:
-                 tensor_out = layers.TimeDistributed(layers.Activation(activation))(tensor_out)
-              else:
-                 tensor_out = layers.Activation(activation)(tensor_out)
-              return tensor_out
-          return _op
-
-      @staticmethod
-      def Dense(units: int, activation: str = "relu", batch_norm: bool = False, time_distributed: bool = False) -> Callable:
-          def _op(tensor: tf.Tensor) -> tf.Tensor:
-              if time_distributed:
-                 tensor_out = layers.TimeDistributed(layers.Dense(units=units, kernel_initializer=tf.initializers.glorot_normal()))(tensor)
-              else:
-                 tensor_out = layers.Dense(units=units, kernel_initializer=tf.initializers.glorot_normal())(tensor)
-              if batch_norm:
-                 if time_distributed:
-                    tensor_out = layers.TimeDistributed(layers.BatchNomalization())(tensor_out)
-                 else:
-                    tensor_out = layers.BatchNormalization()(tensor_out)
-              if time_distributed:
-                 tensor_out = layers.TimeDistributed(layers.Activation(activation))(tensor_out)
-              else:
-                 tensor_out = layers.Activation(activation)(tensor_out)
-              return tensor_out
-          return _op
-
-      @staticmethod
-      def LSTM(units: int) -> Callable:
-          def _op(tensor: tf.Tensor) -> tf.Tensor:
-              if tf.test.is_gpu_available():
-                 return layers.CuDNNLSTM(units=units)
-              return layers.LSTM(units=units)
-          return _op
-
-      @staticmethod
-      def Conv2DNature(batch_norm: bool = False, time_distributed: bool = False) -> Callable:
-          def _op(tensor: tf.Tensor) -> tf.Tensor:
-              conv1 = NetBlocks.Conv2D(filters=32, kernel_size=(8, 8), strides=(4, 4),
-                                       batch_norm=batch_norm, time_distributed=time_distributed)(tensor)
-              conv2 = NetBlocks.Conv2D(filters=64, kernel_size=(4, 4), strides=(2, 2),
-                                       batch_norm=batch_norm, time_distributed=time_distributed)(conv1)
-              conv3 = NetBlocks.Conv2D(filters=64, kernel_size=(3, 3), batch_norm=batch_norm,
-                                       time_distributed=time_distributed)(conv2)
-              flattened = layers.TimeDistributed(layers.Flatten())(conv3) if time_distributed else layers.Flatten()(conv3)
-              return NetBlocks.Dense(units=256, batch_norm=batch_norm, time_distributed=time_distributed)(flattened)
-          return _op
-
-      @staticmethod
-      def Conv2DNatureDueling(batch_norm: bool = False, time_distributed: bool = False) -> Callable:
-          def _op(tensor: tf.Tensor) -> tf.Tensor:
-              conv1 = NetBlocks.Conv2D(filters=32, kernel_size=(8, 8), strides=(4, 4),
-                                       batch_norm=batch_norm, time_distributed=time_distributed)(tensor)
-              conv2 = NetBlocks.Conv2D(filters=64, kernel_size=(4, 4), strides=(2, 2),
-                                       batch_norm=batch_norm, time_distributed=time_distributed)(conv1)
-              conv3 = NetBlocks.Conv2D(filters=64, kernel_size=(3, 3), batch_norm=batch_norm,
-                                       time_distributed=time_distributed)(conv2)
-              flattened = layers.TimeDistributed(layers.Flatten())(conv3) if time_distributed else layers.Flatten()(conv3)
-              return [NetBlocks.Dense(units=256, batch_norm=batch_norm, time_distributed=time_distributed)(flattened),
-                      NetBlocks.Dense(units=256, batch_norm=batch_norm, time_distributed=time_distributed)(flattened)]
-          return _op
-
-      @staticmethod
       def clip_grads_by_norm(gradients: tf.Tensor, clip_norm: float) -> None:
           for i, (grad, var) in enumerate(gradients):
               if grad is not None:
@@ -101,7 +27,83 @@ class NetBlocks:
              return tf.placeholder(dtype=dtype, shape=[], name=name)
           return tf.placeholder(dtype=dtype, shape=[None] + list(shape), name=name)
 
-      class Loss:
+      class layers:
+
+            @staticmethod
+            def Conv2D(filters: int, kernel_size: Tuple[int, int], strides: Tuple[int, int] = (1,1,),
+                       activation: str = "relu", batch_norm: bool = False, time_distributed: bool = False) -> Callable:
+                def _op(tensor: tf.Tensor) -> tf.Tensor:
+                    if time_distributed:
+                       tensor_out = layers.TimeDistributed(layers.Conv2D(filters=filters, kernel_size=kernel_size, strides=strides))(tensor)
+                    else:
+                       tensor_out = layers.Conv2D(filters=filters, kernel_size=kernel_size, strides=strides)(tensor)
+                    if batch_norm:
+                       if time_distributed:
+                          tensor_out = layers.TimeDistributed(layers.BatchNomalization())(tensor_out)
+                       else:
+                          tensor_out = layers.BatchNormalization()(tensor_out)
+                    if time_distributed:
+                       tensor_out = layers.TimeDistributed(layers.Activation(activation))(tensor_out)
+                    else:
+                       tensor_out = layers.Activation(activation)(tensor_out)
+                    return tensor_out
+                return _op
+
+            @staticmethod
+            def Dense(units: int, activation: str = "relu", batch_norm: bool = False, time_distributed: bool = False) -> Callable:
+                def _op(tensor: tf.Tensor) -> tf.Tensor:
+                    if time_distributed:
+                       tensor_out = layers.TimeDistributed(layers.Dense(units=units, kernel_initializer=tf.initializers.glorot_normal()))(tensor)
+                    else:
+                       tensor_out = layers.Dense(units=units, kernel_initializer=tf.initializers.glorot_normal())(tensor)
+                    if batch_norm:
+                       if time_distributed:
+                          tensor_out = layers.TimeDistributed(layers.BatchNomalization())(tensor_out)
+                       else:
+                          tensor_out = layers.BatchNormalization()(tensor_out)
+                    if time_distributed:
+                       tensor_out = layers.TimeDistributed(layers.Activation(activation))(tensor_out)
+                    else:
+                       tensor_out = layers.Activation(activation)(tensor_out)
+                    return tensor_out
+                return _op
+
+            @staticmethod
+            def LSTM(units: int) -> Callable:
+                def _op(tensor: tf.Tensor) -> tf.Tensor:
+                    if tf.test.is_gpu_available():
+                       return layers.CuDNNLSTM(units=units)
+                    return layers.LSTM(units=units)
+                return _op
+
+            @staticmethod
+            def Conv2DNature(batch_norm: bool = False, time_distributed: bool = False) -> Callable:
+                def _op(tensor: tf.Tensor) -> tf.Tensor:
+                    conv1 = NetBlocks.Conv2D(filters=32, kernel_size=(8, 8), strides=(4, 4),
+                                             batch_norm=batch_norm, time_distributed=time_distributed)(tensor)
+                    conv2 = NetBlocks.Conv2D(filters=64, kernel_size=(4, 4), strides=(2, 2),
+                                             batch_norm=batch_norm, time_distributed=time_distributed)(conv1)
+                    conv3 = NetBlocks.Conv2D(filters=64, kernel_size=(3, 3), batch_norm=batch_norm,
+                                             time_distributed=time_distributed)(conv2)
+                    flattened = layers.TimeDistributed(layers.Flatten())(conv3) if time_distributed else layers.Flatten()(conv3)
+                    return NetBlocks.Dense(units=256, batch_norm=batch_norm, time_distributed=time_distributed)(flattened)
+                return _op
+
+            @staticmethod
+            def Conv2DNatureDueling(batch_norm: bool = False, time_distributed: bool = False) -> Callable:
+                def _op(tensor: tf.Tensor) -> tf.Tensor:
+                    conv1 = NetBlocks.Conv2D(filters=32, kernel_size=(8, 8), strides=(4, 4),
+                                             batch_norm=batch_norm, time_distributed=time_distributed)(tensor)
+                    conv2 = NetBlocks.Conv2D(filters=64, kernel_size=(4, 4), strides=(2, 2),
+                                             batch_norm=batch_norm, time_distributed=time_distributed)(conv1)
+                    conv3 = NetBlocks.Conv2D(filters=64, kernel_size=(3, 3), batch_norm=batch_norm,
+                                             time_distributed=time_distributed)(conv2)
+                    flattened = layers.TimeDistributed(layers.Flatten())(conv3) if time_distributed else layers.Flatten()(conv3)
+                    return [NetBlocks.Dense(units=256, batch_norm=batch_norm, time_distributed=time_distributed)(flattened),
+                            NetBlocks.Dense(units=256, batch_norm=batch_norm, time_distributed=time_distributed)(flattened)]
+                return _op
+
+      class losses:
 
             @staticmethod
             def huber(predicted: tf.Tensor, labels: tf.Tensor) -> tf.Tensor:
