7b124cc1284d0fdfcc457b68025145b69b3a7202
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-10-19 02:32:08 +0530

    Update __init__.py

--- symbiosis/symbiosis/Agents/Utilities/__init__.py
+++ symbiosis/symbiosis/Agents/Utilities/__init__.py
@@ -5,6 +5,7 @@ from .reward_manager import RewardManager
 from .inventory import Inventory
 from .lego import NetBlocks
 from .flow import LucasKanadeFlow, GunnerFarnebackFlow
+from .event_writer import EventWriter
 
 __all__ = ["LRScheduler", "Progress", "Exceptions", "RewardManager", "NetBlocks", "Inventory", "LucasKanadeFlow",
            "GunnerFarnebackFlow", "BetaScheduler", "EpsilonGreedyScheduler"]
