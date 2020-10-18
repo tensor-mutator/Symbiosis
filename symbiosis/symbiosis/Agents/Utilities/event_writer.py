da546f5924d33af59dc9b34d9660b2b7e3dfcf96
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-10-19 02:44:45 +0530

    Update event_writer.py

--- symbiosis/symbiosis/Agents/Utilities/event_writer.py
+++ symbiosis/symbiosis/Agents/Utilities/event_writer.py
@@ -1,13 +1,26 @@
 import tensorflow.compat.v1 as tf
+from typing import Callable
+from .progress import Progress
+from .scheduler import Scheduler
 
-class EventWriter:
+class RegisterWriter:
 
-      def __init__(self, tag: str, write: bool, writer: tf.summary.FileWriter) -> None:
+      @staticmethod
+      def registerwriter(func) -> Callable:
+          def inner(inst: "<Scheduler inst>") -> float:
+              val = func(inst)
+              summary = tf.Summary()
+              summary.value.add(tag=inst._tag, simple_value=val)
+              inst._writer.add_summary(summary, getattr(inst._progress, inst._y))
+              return val
+          return inner
+
+class EventWriter(RegisterWriter):
+
+      def __init__(self, tag: str, write: bool, writer: tf.summary.FileWriter, progress: Progress,
+                   y: str) -> None:
           self._tag = tag
           self._write = write
           self._writer = writer
-
-      def write(self, x: int, y: float) -> None:
-          summary = tf.Summary()
-          summary.value.add(tag=self_tag, simple_value=y)
-          self._writer.add_summary(summary, x)
+          self._progress = progress
+          self._y = y
