ce434bbda194d01c2af39d16433836d0868aa52e
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2021-03-17 02:33:25 +0530

    Create tree.py

---
+++ symbiosis/symbiosis/Agents/Utilities/tree.py
@@ -0,0 +1,31 @@
+"""
+@author: Debajyoti Raychaudhuri
+
+Implements game tree to run Monte-Carlo Tree Search (MCTS) on
+"""
+
+from threading import Lock
+from collections import defaultdict
+from dataclasses import dataclass, field
+from typing import List
+
+@dataclass
+class Node:
+
+      edges: List = field(default_factory=lambda: defaultdict(Edge))
+      sum_n: int = 0
+
+@dataclass
+class Edge:
+
+      n: int = 0
+      w: float = 0
+      q: float = 0
+      p: List = field(default_factory=list)
+
+class Tree:
+
+      def __init__(self, depth: int, branching_factor: int) -> None:
+          self._lock = Lock()
+          self._depth = depth
+          self._branching_factor = branching_factor
