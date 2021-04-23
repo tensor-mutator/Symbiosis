50741b54fc5c3f2008a877c75ca1c7d96097535b
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2021-04-23 21:44:50 +0530

    Update agz.py

--- symbiosis/symbiosis/Agents/AGZ/agz.py
+++ symbiosis/symbiosis/Agents/AGZ/agz.py
@@ -1,7 +1,7 @@
 """
 @author: Debajyoti Raychaudhuri
 
-A precise implementation of an Alpha Go Zero agent
+A precise implementation of an AlphaGoZero agent
 """
 
 import tensorflow.compat.v1 as tf
