f1e7519a0eab61a11833c1a9698eb2ee6b2a3192
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2021-05-09 19:31:22 +0530

    Update tree.py

--- symbiosis/symbiosis/Agents/Utilities/tree.py
+++ symbiosis/symbiosis/Agents/Utilities/tree.py
@@ -4,7 +4,7 @@
 Implements game tree to run Monte-Carlo Search on
 """
 
-from multiprocessing import Lock, Array, Value
+from multiprocessing import Lock, Queue
 from collections import defaultdict
 from dataclasses import dataclass, field
 from typing import List, Iterator, Dict
@@ -36,11 +36,11 @@ class Tree:
           Implements a game tree
       """
 
-      def __init__(self, virtual_loss: float) -> None:
+      def __init__(self, virtual_loss: float, n_procs: int) -> None:
           self._virtual_loss = virtual_loss
           self._lock = defaultdict(Lock)
           self._tree = defaultdict(Node)
-          self._shared_mem = defaultdict(lambda: dict(sum_n=Value('d'), edges=defaultdict(lambda: Array('f', 4))))
+          self._n_procs = n_procs
 
       def __iter__(self) -> Iterator:
           return list(self._tree.keys())
@@ -51,53 +51,71 @@ class Tree:
       def __contains__(self, state: str) -> bool:
           return state in self._tree.keys()
 
-      def expand(self, state: str, policy: List[float], actions: List[str]) -> None:
+      def reset(self) -> None:
+          self._tree = defaultdict(Node)
+
+      def _recv_update(self, queues: List[Queue]) -> None:
+          for queue in queues:
+              if not queue.empty():
+                 updated_meta = queue.get()
+                 self._tree = updated_meta["tree"]
+                 self._lock = updted_meta["lock"]
+                 break
+
+      def _send_update(self, queue: Queue) -> None:
+          meta = dict(tree=self._tree, lock=self._lock)
+          for _ in range(self._n_procs-1):
+              queue.put(meta)
+
+      def is_missing(self, state: str, lock: Lock, recv_queues: List[Queue]) -> bool:
+          _lock = lock()
+          _lock.acquire()
+          self._recv_update(recv_queues)
+          if state in self._tree:
+             _lock.release()
+             return False
+          return True
+
+      def expand(self, state: str, policy: List[float], actions: List[str], lock: Lock, send_queue: Queue) -> None:
           """
               Expands the tree with a node
           """
 
+          _lock = lock()
           normalizing_factor = sum(policy)+1e-08
           actions_mem = self._shared_mem[state]["edges"]
           for action, p in zip(actions, policy):
               self._tree[state].edges[action].p = p/normalizing_factor
               actions_mem[action][3] = p/normalizing_factor
+          self._send_update(send_queue)
+          _lock.release()
 
-      def simulate(self, state: str, action: str) -> None:
+      def simulate(self, state: str, action: str, send_queue: Queue, recv_queues: List[Queue]) -> None:
           """
               Traverses a node while applying virtual loss
           """
 
           with self._lock(state):
+               self._recv_update(recv_queues)
                node = self._tree[state]
-               sum_n = self._shared_mem[state]["sum_n"].value
-               sum_n += self._virtual_loss
-               node.sum_n = sum_n
+               node.sum_n += self._virtual_loss
                edge = node.edges[action]
-               n, w, q, _ = self._shared_mem[state]["edges"][action]
-               n += self._virtual_loss
-               edge.n = n
-               w += -self._virtual_loss
-               edge.w = w
-               q = w/n
-               edge.q = q
-               self._shared_mem[state]["edges"][action][:-1] = [n, w, q]
+               edge.n += self._virtual_loss
+               edge.w += -self._virtual_loss
+               edge.q = edge.w/edge.n
+               self._send_update(send_queue)
 
-      def backpropagate(self, state: str, value: float) -> None:
+      def backpropagate(self, state: str, value: float, send_queue: Queue, recv_queues: List[Queue]) -> None:
           """
               Updates the visitation frequency and the action value of a node
           """
 
           with self._lock(state):
+               self._recv_update(recv_queues)
                node = self._tree[state]
-               sum_n = self._shared_mem[state]["sum_n"].value
-               sum_n += -self._virtual_loss + 1
-               node.sum_n = sum_n
+               node.sum_n += -self._virtual_loss + 1
                edge = node.edges[action]
-               n, w, q, _ = self._shared_mem[state]["edges"][action]
-               n += -self._virtual_loss + 1
-               edge.n = n
-               w += self._virtual_loss + value
-               edge.w = w
-               q = w/n
-               edge.q = q
-               self._shared_mem[state]["edges"][action][:-1] = [n, w, q]
+               edge.n += -self._virtual_loss + 1
+               edge.w += self._virtual_loss + value
+               edge.q = edge.w/edge.n
+               self._send_update(send_queue)
