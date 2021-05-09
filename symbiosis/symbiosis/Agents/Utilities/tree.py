5a9de782829976e622eb9e8aadb22d302cfae7af
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2021-05-09 23:45:39 +0530

    Update tree.py

--- symbiosis/symbiosis/Agents/Utilities/tree.py
+++ symbiosis/symbiosis/Agents/Utilities/tree.py
@@ -36,12 +36,10 @@ class Tree:
           Implements a game tree
           
           Arguments:
-                 virtual_loss (float): Virtual loss to be used during traversal for simulation
                  n_procs (int): No of simultaneous processes this tree is going to be used with
       """
 
-      def __init__(self, virtual_loss: float, n_procs: int) -> None:
-          self._virtual_loss = virtual_loss
+      def __init__(self, n_procs: int) -> None:
           self._n_procs = n_procs
           self._lock = defaultdict(Lock)
           self._tree = defaultdict(Node)
@@ -94,7 +92,8 @@ class Tree:
           self._send_update(send_queue, data=dict(state={state: self._tree[state]}, lock=dict(state=self._lock[state])))
           lock.release()
 
-      def simulate(self, state: str, action: str, send_queue: Queue, recv_queues: List[Queue]) -> None:
+      def simulate(self, state: str, action: str, virtual_loss: float,
+                   send_queue: Queue, recv_queues: List[Queue]) -> None:
           """
               Traverses a node while applying virtual loss
           """
@@ -102,14 +101,15 @@ class Tree:
           with self._lock(state):
                self._recv_update(recv_queues)
                node = self._tree[state]
-               node.sum_n += self._virtual_loss
+               node.sum_n += virtual_loss
                edge = node.edges[action]
-               edge.n += self._virtual_loss
-               edge.w += -self._virtual_loss
+               edge.n += virtual_loss
+               edge.w += -virtual_loss
                edge.q = edge.w/edge.n
                self._send_update(send_queue, data=dict(state={state: node}, lock=dict()))
 
-      def backpropagate(self, state: str, value: float, send_queue: Queue, recv_queues: List[Queue]) -> None:
+      def backpropagate(self, state: str, value: float, virtual_loss: float,
+                        send_queue: Queue, recv_queues: List[Queue]) -> None:
           """
               Updates the visitation frequency and the action value of a node
           """
@@ -117,9 +117,9 @@ class Tree:
           with self._lock(state):
                self._recv_update(recv_queues)
                node = self._tree[state]
-               node.sum_n += -self._virtual_loss + 1
+               node.sum_n += -virtual_loss + 1
                edge = node.edges[action]
-               edge.n += -self._virtual_loss + 1
-               edge.w += self._virtual_loss + value
+               edge.n += -virtual_loss + 1
+               edge.w += virtual_loss + value
                edge.q = edge.w/edge.n
                self._send_update(send_queue, data=dict(state={state: node}, lock=dict()))
