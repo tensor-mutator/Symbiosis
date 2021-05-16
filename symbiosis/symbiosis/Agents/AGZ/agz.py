c710f47d113e4ae32aed193a91c3c10720c350a3
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2021-05-16 16:25:24 +0530

    Update agz.py

--- symbiosis/symbiosis/Agents/AGZ/agz.py
+++ symbiosis/symbiosis/Agents/AGZ/agz.py
@@ -5,39 +5,39 @@ A precise implementation of an AlphaGo Zero agent
 """
 
 import tensorflow.compat.v1 as tf
-from .network import *
+from typing import Dict
+from .network import AGZChessNet
 from ..agent import Agent
 from ..flow_base import Flow
-from ..network_base import NetworkBaseMCTS
-from ..Utilities import Tree, Progress
+from ..network_base import NetworkBaseAGZ
+from ..Utilities import Tree, Progress, MCTS
 from ...environment import Environment
 from ...config import config
 
-@Agent.track(AGZNet)
+@Agent.track(AGZChessNet)
 class AGZ(Agent):
 
-      def __init__(self, env: Environment, network: NetworkBaseMCTS = AGZNet, config: bin = config.DEFAULT,
+      def __init__(self, env: Environment, network: NetworkBaseAGZ = AGZChessNet, config: bin = config.DEFAULT,
                    flow: Flow = None, **hyperparams) -> None:
           self._env = env
           self._config = config
           self._flow = flow
-          self._read_params(hyperparams)
           self._alias = self._define_alias(network.type, hyperparams)
           self._progress = self.load_progress()
           self._alias = "AGZ"
-          self._session = self._build_network_graph(network, hyperparams)
+          self._mcts = self._initiate_tree(network, hyperparams)
 
-      def _build_network_graph(self, network: NetworkBaseMCTS, hyperparams: Dict) -> tf.Session:
-          self._graph = tf.Graph()
-          session = tf.Session(graph=self._graph, config=self.ConfigProto)
-          with self._graph.as_default():
-               optional_network_params = self._get_optional_network_params(hyperparams)
-               self._local_network = network(self._env.state.shape, self._trace, self._env.action.size,
-                                             **optional_network_params, scope="local")
-               self._target_network = network(self._env.state.shape, self._trace, self._env.action.size,
-                                              **optional_network_params, scope="target")
-               self._update_ops = self._initiate_update_ops("local", "target")
-          return session
+      def _initiate_tree(self, network: NetworkBaseAGZ, hyperparams: Dict) -> MCTS:
+          virtual_loss = hyperparams.get("virtual_loss", 3)
+          n_threads = hyperparams.get("n_threads", 3)
+          n_simulations = hyperparams.get("n_simulations", 16)
+          mcts = MCTS(env=self._env, tree=Tree(), virtual_loss=virtual_loss, n_threads=n_threads,
+                      n_simulations=n_simulations, network=network, **hyperparams)
+          return mcts
 
-      def __del__(self) -> None:
-          self._session.close()
+      def action(self, env: Environment) -> Any:
+          
+
+      @Agent.record
+      def state(self, env: Environment) -> np.ndarray:
+          return env.state.frame
