ecddd57eca3588628aba9e11b276ddb9047b3816
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2021-05-17 19:09:50 +0530

    Update agz.py

--- symbiosis/symbiosis/Agents/AGZ/agz.py
+++ symbiosis/symbiosis/Agents/AGZ/agz.py
@@ -7,6 +7,7 @@ A precise implementation of an AlphaGo Zero agent
 import tensorflow.compat.v1 as tf
 from typing import Dict
 from collections import deque
+import numpy as np
 from .network import AGZChessNet
 from ..agent import Agent
 from ..flow_base import Flow
@@ -25,19 +26,29 @@ class AGZ(Agent):
           self._flow = flow
           self._alias = network.type
           self._progress = self.load_progress()
-          self._mcts = self._initiate_tree(network, hyperparams)
+          self._mcts, self._tree = self._initiate_tree(network, hyperparams)
           self._self_play_buffer = deque()
 
       def _initiate_tree(self, network: NetworkBaseAGZ, hyperparams: Dict) -> MCTS:
           virtual_loss = hyperparams.get("virtual_loss", 3)
           n_threads = hyperparams.get("n_threads", 3)
           n_simulations = hyperparams.get("n_simulations", 16)
-          mcts = MCTS(env=self._env, tree=Tree(), virtual_loss=virtual_loss, n_threads=n_threads,
+          tree = Tree()
+          mcts = MCTS(env=self._env, tree=tree, virtual_loss=virtual_loss, n_threads=n_threads,
                       n_simulations=n_simulations, network=network, **hyperparams)
-          return mcts
+          return mcts, tree
 
       def action(self, env: Environment) -> Any:
+          value = self._mcts.search()
+          policy = self._policy_target(env)
           
+
+      def _policy_target(self, env: Enviornment) -> np.ndarray:
+          policy = np.zeros(env.action.size, dtype=np.float32)
+          for action, stat in self._tree[env.state.observation].edges.items():
+              policy[env.action.move2index(action)] = stat.n
+          policy = policy/np.sum(policy)
+          return policy
 
       @Agent.record
       def state(self, env: Environment) -> np.ndarray:
