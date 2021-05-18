9ab43ffd4fb083bd808a51776b41ee697bb553b6
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2021-05-18 19:47:47 +0530

    Update config.py

--- symbiosis/symbiosis/config.py
+++ symbiosis/symbiosis/config.py
@@ -3,20 +3,21 @@ from enum import IntEnum, unique
 @unique
 class config(IntEnum):
 
-      DEFAULT: bin = 0b0000000000000
-      VERBOSE_LITE: bin = 0b0000000000001
-      VERBOSE_HEAVY: bin = 0b0000000000010
-      LOG_SUMMARY: bin = 0b0000000000100
-      REWARD_EVENT: bin = 0b0000000001000
-      LOSS_EVENT: bin = 0b0000000010000
-      EPSILON_EVENT: bin = 0b0000000100000
-      BETA_EVENT: bin = 0b0000001000000
-      LR_EVENT: bin = 0b0000010000000
-      SAVE_WEIGHTS: bin = 0b0000100000000
-      LOAD_WEIGHTS: bin = 0b0001000000000
-      SAVE_FRAMES: bin = 0b0010000000000
-      SAVE_FLOW: bin = 0b0100000000000
-      NOHUP: bin = 0b1000000000000
+      DEFAULT: bin = 0b00000000000000
+      VERBOSE_LITE: bin = 0b00000000000001
+      VERBOSE_HEAVY: bin = 0b00000000000010
+      LOG_SUMMARY: bin = 0b00000000000100
+      REWARD_EVENT: bin = 0b00000000001000
+      LOSS_EVENT: bin = 0b00000000010000
+      EPSILON_EVENT: bin = 0b00000000100000
+      BETA_EVENT: bin = 0b00000001000000
+      LR_EVENT: bin = 0b00000010000000
+      TAU_EVENT: bin = 0b00000100000000
+      SAVE_WEIGHTS: bin = 0b00001000000000
+      LOAD_WEIGHTS: bin = 0b00010000000000
+      SAVE_FRAMES: bin = 0b00100000000000
+      SAVE_FLOW: bin = 0b01000000000000
+      NOHUP: bin = 0b10000000000000
 
       @staticmethod
       def show() -> None:
@@ -29,6 +30,7 @@ class config(IntEnum):
           print("config.EPSILON_EVENT")
           print("config.BETA_EVENT")
           print("config.LR_EVENT")
+          print("config.TAU_EVENT")
           print("config.SAVE_WEIGHTS")
           print("config.LOAD_WEIGHTS")
           print("config.SAVE_FRAMES")
