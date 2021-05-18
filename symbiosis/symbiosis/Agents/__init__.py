edb4d29968937c1af9e3566fbc387556524af7b0
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2021-05-18 12:49:26 +0530

    Update __init__.py

--- symbiosis/symbiosis/Agents/__init__.py
+++ symbiosis/symbiosis/Agents/__init__.py
@@ -1,6 +1,6 @@
 from .DQN.ddqn import DDQN
 from .DQN.network import DQNNet, DRQNNet, DuelingDQNNet
-from .agent import Agent
+from .agent import Agent, AgentForked
 from .flow_base import Flow
 from .network_base import NetworkBaseDQN
 from .Utilities.lego import NetBlocks
