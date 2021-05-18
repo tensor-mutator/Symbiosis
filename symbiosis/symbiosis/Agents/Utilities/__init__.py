eb77cd85437753d43fc04e4e200e5447cdf55937
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2021-05-18 22:48:00 +0530

    Update __init__.py

--- symbiosis/symbiosis/Agents/Utilities/__init__.py
+++ symbiosis/symbiosis/Agents/Utilities/__init__.py
@@ -1,12 +1,12 @@
 from .scheduler import LRScheduler, BetaScheduler, EpsilonScheduler, TauScheduler, Scheduler
 from .progress import Progress, ProgressDQN
 from .exceptions import *
-from .reward_manager import RewardManager
+from .reward_manager import RewardManager, ELOManager
 from .inventory import Inventory
 from .lego import NetBlocks
 from .flow import LucasKanadeFlow, GunnarFarnebackFlow
 from .event_writer import EventWriter
 from .tree import Tree
 
-__all__ = ["LRScheduler", "Progress", "ProgressDQN", "Exceptions", "RewardManager", "NetBlocks", "Inventory", "LucasKanadeFlow",
+__all__ = ["LRScheduler", "Progress", "ProgressDQN", "Exceptions", "RewardManager", "ELOManager", "NetBlocks", "Inventory", "LucasKanadeFlow",
            "GunnarFarnebackFlow", "BetaScheduler", "EpsilonScheduler", "TauScheduler", "Tree"]
