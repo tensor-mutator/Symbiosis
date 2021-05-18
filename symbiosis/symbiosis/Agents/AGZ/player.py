ec291251dae7b0b316cdb851ea50dc0fe8d6d1c4
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2021-05-18 15:33:39 +0530

    Update player.py

--- symbiosis/symbiosis/Agents/AGZ/player.py
+++ symbiosis/symbiosis/Agents/AGZ/player.py
@@ -1,14 +1,17 @@
 from typing import Callable
+from collections import deque
 from ..agent import AgentForked
 from ..Utilities import Tree, Progress, MCTS, TauScheduler
 from ...environment import Environment
 
 class Player(AgentForked):
 
-      def __init__(self, env: Environment, predict_p_v: Callable, **hyperparams) -> None:
+      def __init__(self, env: Environment, predict_p_v: Callable, buffer: deque, alias: str, **hyperparams) -> None:
           self._env = env
           self._read_params(hyperparams)
           self._mcts, self._tree = self._initiate_tree(predict_p_v, hyperparams)
+          self._buffer = buffer
+          self._alias = alias
 
       def _initiate_tree(self, predict_p_v: Callable, hyperparams: Dict) -> Tuple[MCTS, Tree]:
           virtual_loss = hyperparams.get("virtual_loss", 3)
@@ -32,6 +35,27 @@ class Player(AgentForked):
           policy = self._policy_target()
           if value <= self._resign_value and env.n_halfmoves > self._min_resign_moves:
              return None
-          self._self_play_buffer.append((env.state.canonical, policy, value))
+          self._buffer.append((env.state.canonical, policy, value))
           action_idx = np.random.choice(np.arange(env.action.size), p=self._policy_with_temperature(policy))
           return env.action.labels[action_idx]
+
+      def _policy_target(self) -> np.ndarray:
+          policy = np.zeros(self._env.action.size, dtype=np.float32)
+          for action, stat in self._tree[self._env.state.observation].edges.items():
+              policy[self._env.action.move2index(action)] = stat.n
+          policy = policy/np.sum(policy)
+          return policy
+
+      def _policy_with_temperature(self, policy: np.ndarray) -> np.ndarray:
+          if self._tau_scheduler.tau < 0.1:
+             policy = np.zeros(self._env.action.size, dtype=np.float32)
+             policy[np.argmax(policy)] = 1
+             return policy
+          policy = np.power(policy, 1/self._tau_scheduler.tau)
+          return policy/np.sum(policy)
+
+      def save(self, workspace: str, alias: str) -> None:
+          self._tree.save(workspace, alias)
+
+      def load(self, workspace: str, alias: str) -> None:
+          self._tree.load(workspace, alias)
