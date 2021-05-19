6f2b1bd72d64523980749e61cf76ab18312f29d3
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2021-05-19 19:14:32 +0530

    Update network.py

--- symbiosis/symbiosis/Agents/AGZ/network.py
+++ symbiosis/symbiosis/Agents/AGZ/network.py
@@ -36,4 +36,6 @@ class AGZChessNet(NetworkBaseAGZ):
       def __init__(self, state_shape: Tuple[int, int, int], action_size: int, **params) -> Pipeline:
           placeholders = dict(state=NetBlocks.placeholder(state_shape), p_target=NetBlocks.placeholder(shape=[action_size]),
                               v_target=NetBlocks.placeholder(shape=[], dtype=tf.float32))
-          return Pipeline(shape_X=state_shape, shape_y=dict(p_target=action_size, v_target=1), model=AGZChessNet.AGZChessNetModel, **params)
+          return Pipeline(meta_X=dict(shape=state_shape, dtype=tf.float32), meta_y=dict(p_target=dict(shape=action_size, dtype=tf.float32),
+                                                                                        v_target=dict(shape=1, dtype=tf.float32)),
+                          model=AGZChessNet.AGZChessNetModel, **params)
