24d025894ce6e36543c9d6f1b255bb688d98d0bf
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2021-05-19 18:00:42 +0530

    Update network.py

--- symbiosis/symbiosis/Agents/AGZ/network.py
+++ symbiosis/symbiosis/Agents/AGZ/network.py
@@ -1,24 +1,28 @@
 import tensorflow.compat.v1 as tf
 import tensorflow.compat.v1.keras.regularizers as regularizers
 import tensorflow.compat.v1.keras.layers as layers
-from typing import Tuple
+from typing import Tuple, List
 from ..network_base import NetworkBaseAGZ
-from ..Utilities import NetBlocks
+from ..Utilities import NetBlocks, Pipeline
 
 class AGZChessNet(NetworkBaseAGZ):
 
       type: str = "AGZChess"
 
-      def __init__(self, state_shape: Tuple[int, int, int], action_size: int, **params) -> None:
-          self._state = NetBlocks.placeholder(state_shape)
+      def __init__(self, state_shape: Tuple[int, int, int], action_size: int, **params) -> Pipeline:
+          placeholders = dict(state=NetBlocks.placeholder(state_shape), p_target=NetBlocks.placeholder(shape=[action_size]),
+                              v_target=NetBlocks.placeholder(shape=[], dtype=tf.float32))
+          return Pipeline(placeholders, self._model)
+
+      def _model(placeholders: Dict) -> None:
+          self._state = placeholders.get("state")
           p_out, self._v_predicted = NetBlocks.NN.ChessNet(batch_norm=True)(self._state)
           logits =  NetBlocks.layers.Dense(units=action_size, kernel_regularizer=regularizers.l2(1e-4))(p_out)
           self._p_predicted = layers.Activation("softmax")(logits)
-          self._grad = self._build_training_ops(action_size, logits, **params)
+          self._grad = self._build_training_ops(placeholders, logits, **params)
 
-      def _build_training_ops(self, action_size: int, logits: tf.Tensor, **params) -> tf.Tensor:
-          self._p_target = NetBlocks.placeholder(shape=[action_size])
-          self._v_target = NetBlocks.placeholder(shape=[], dtype=tf.float32)
+      def _build_training_ops(self, placeholders: Dict, logits: tf.Tensor, **params) -> tf.Tensor:
+          self._p_target, self._v_target = placeholders.get("p_target"), placeholders.get("v_target")
           self._learning_rate = NetBlocks.placeholder(shape="scalar")
           policy_error = tf.losses.softmax_cross_entropy(onehot_labels=self._p_target, logits=logits, weights=params.get("policy_weights", 1.25))
           value_error = tf.losses.mean_squared_error(labels=self._v_target, predictions=self._v_predicted, weights=params.get("value_weights", 1.0))
