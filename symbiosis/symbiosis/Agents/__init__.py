c7bc161e6e4b0adcc54528ff613a9bab28cbb693
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2021-05-20 17:13:04 +0530

    Update __init__.py

--- symbiosis/symbiosis/Agents/__init__.py
+++ symbiosis/symbiosis/Agents/__init__.py
@@ -2,6 +2,7 @@ from .DQN.ddqn import DDQN
 from .DQN.network import DQNNet, DRQNNet, DuelingDQNNet
 from .AGZ.agz import AGZ
 from .AGZ.network import AGZChessNet
+from .AGZ.player import Player
 from .agent import Agent, AgentForked
 from .model import Model
 from .flow_base import Flow
@@ -9,5 +10,5 @@ from .network_base import NetworkBaseDQN
 from .Utilities.lego import NetBlocks
 from .Utilities.flow import GunnarFarnebackFlow, LucasKanadeFlow
 
-__all__ = ["DDQN", "NetworkBaseDQN", "DQNNet", "DRQNNet", "AGZ", "AGZChessNet", "DuelingDQNNet", "NetBlocks", "Flow", "LucasKanadeFlow",
-           "GunnerFarnebackFlow"]
+__all__ = ["DDQN", "NetworkBaseDQN", "DQNNet", "DRQNNet", "AGZ", "AGZChessNet", "Player", "DuelingDQNNet",
+           "NetBlocks", "Flow", "LucasKanadeFlow", "GunnerFarnebackFlow"]
