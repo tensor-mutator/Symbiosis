a8f4873d568d879c0eaf2c9d10a4f95c35c88a7c
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2021-05-22 21:44:22 +0530

    Update metrics.py

--- symbiosis/symbiosis/Agents/Utilities/metrics.py
+++ symbiosis/symbiosis/Agents/Utilities/metrics.py
@@ -5,7 +5,7 @@ class Metrics:
       @staticmethod
       def MicroPrecision(y: tf.Tensor, y_hat: tf.Tensor) -> tf.Tensor:
           y_pred = tf.argmax(y_hat, axis=-1)
-          y_cap = tf.one_hot(indices=y_pred, depth=tf.shape(y)[-1], dtype=tf.int32)
+          y_cap = tf.one_hot(indices=y_pred, depth=tf.shape(y)[-1], dtype=y_hat.dtype)
           TP_plus_FP = tf.reduce_sum(y_cap)
           TP = tf.reduce_sum(tf.where(tf.cast(y_cap, tf.bool), y, tf.zeros_like(y_cap)))
           return TP/TP_plus_FP
@@ -13,7 +13,7 @@ class Metrics:
       @staticmethod
       def MicroRecall(y: tf.Tensor, y_hat: tf.Tensor) -> tf.Tensor:
           y_pred = tf.argmax(y_hat, axis=-1)
-          y_cap = tf.one_hot(indices=y_pred, depth=tf.shape(y)[-1], dtype=tf.int32)
+          y_cap = tf.one_hot(indices=y_pred, depth=tf.shape(y)[-1], dtype=y_hat.dtype)
           TP_plus_FN = tf.reduce_sum(y)
           TP = tf.reduce_sum(tf.where(tf.cast(y, tf.bool), y_cap, tf.zeros_like(y)))
           return TP/TP_plus_FN
@@ -27,7 +27,7 @@ class Metrics:
       @staticmethod
       def MacroPrecision(y: tf.Tensor, y_hat: tf.Tensor) -> tf.Tensor:
           y_pred = tf.argmax(y_hat, axis=-1)
-          y_cap = tf.one_hot(indices=y_pred, depth=tf.shape(y)[-1], dtype=tf.int32)
+          y_cap = tf.one_hot(indices=y_pred, depth=tf.shape(y)[-1], dtype=y_hat.dtype)
           TP_plus_FP = tf.reduce_sum(y_cap, axis=0)
           TP = tf.reduce_sum(tf.where(tf.cast(y_cap, tf.bool), y, tf.zeros_like(y_cap)), axis=0)
           return tf.reduce_mean(TP/TP_plus_FP)
@@ -35,7 +35,7 @@ class Metrics:
       @staticmethod
       def MacroRecall(y: tf.Tensor, y_hat: tf.Tensor) -> tf.Tensor:
           y_pred = tf.argmax(y_hat, axis=-1)
-          y_cap = tf.one_hot(indices=y_pred, depth=tf.shape(y)[-1], dtype=tf.int32)
+          y_cap = tf.one_hot(indices=y_pred, depth=tf.shape(y)[-1], dtype=y_hat.dtype)
           TP_plus_FN = tf.reduce_sum(y, axis=0)
           TP = tf.reduce_sum(tf.where(tf.cast(y, tf.bool), y_cap, tf.zeros_like(y)), axis=0)
           return tf.reduce_mean(TP/TP_plus_FN)
@@ -49,5 +49,5 @@ class Metrics:
       @staticmethod
       def HammingLoss(y: tf.Tensor, y_hat: tf.Tensor) -> tf.Tensor:
           y_pred = tf.argmax(y_hat, axis=-1)
-          y_cap = tf.one_hot(indices=y_pred, depth=tf.shape(y)[-1], dtype=tf.int32)
+          y_cap = tf.one_hot(indices=y_pred, depth=tf.shape(y)[-1], dtype=y_hat.dtype)
           return tf.reduce_sum(tf.cast(tf.not_equal(y, y_cap), tf.int32))/tf.reduce_sum(tf.ones_like(y_cap))
