4d21d3fa3e7b982c4329eadb45eefd96186e746a
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2021-05-23 15:09:20 +0530

    Update __init__.py

--- symbiosis/symbiosis/Agents/__init__.py
+++ symbiosis/symbiosis/Agents/__init__.py
@@ -3,7 +3,7 @@ from .DQN.network import DQNNet, DRQNNet, DuelingDQNNet
 from .AGZ.agz import AGZ
 from .AGZ.network import AGZChessNet
 from .AGZ.player import Player
-from .agent import Agent, AgentForked
+from .agent import Agent, MarkovPlayer
 from .model import Model
 from .flow_base import Flow
 from .network_base import NetworkBaseDQN
